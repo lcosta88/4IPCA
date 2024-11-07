5bb7e53c39e5b140f258242f31cc2a69dc4e5f35
Jonatan203101 <a23101@alunos.ipca.pt>
2024-11-07 18:36:13 +0100

    Calculadora class

--- .vscode/Calculadora_MVC/Calculadora_Class.cpp
+++ .vscode/Calculadora_MVC/Calculadora_Class.cpp
@@ -1,6 +1,52 @@
-#inculde <iostream>
+#include <iostream>
+using namespace std;
 
-class Adicao {
+class Calculadora {
     public:
         float resultado;
+        float a;
+        float b;
+};
+
+int main(){
+    float a, b;
+    int opcao;
+    Calculadora adicao;
+    Calculadora substraçao;
+    Calculadora divisao;
+    Calculadora mutiplicacao;
+
+    cout << "Escolhe uma opcao: ";
+    cin >> opcao;
+    cout << "Escolhe um numero: ";
+    cin >> a;
+    cout << "Escolhe outro numero: ";
+    cin >> b;
+    
+    switch (opcao){
+        case 1:
+        adicao.resultado = a + b;
+        cout << adicao.resultado;
+        break;
+        
+        case 2:
+        adicao.resultado = a - b;
+        cout << adicao.resultado;
+        break;
+        
+        case 3:
+        adicao.resultado = a / b;
+        cout << adicao.resultado;
+        break;
+        
+        case 4:
+        adicao.resultado = a * b;
+        cout << adicao.resultado;
+        break;
+        
+        default:
+        cout <<"Escolha invalida";
+    }
+    return 0;
+    
 }
